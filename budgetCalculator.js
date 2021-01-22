d93f2ae49a21519cb55d2b572a39fe3316e8a574
git-sohel <emdsohel@gmail.com>
2021-01-22 21:44:55 +0600

    ok

--- budgetCalculator.js
+++
@@ -1,9 +0,0 @@
-function budgetCalculator(watch, phone, laptop) {
-    var watch = 50;
-    var phone = 100;
-    var laptop = 500;
-    var total = watch + phone + laptop;
-    return total;
-
-}
-console.log(total);
