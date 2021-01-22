3c31f34aefe53b64b34e6b504488636bb84c56f8
git-sohel <emdsohel@gmail.com>
2021-01-22 18:22:26 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,8 @@
+
+var meter = 1000;
+function kilometerToMeter (kilometer) {
+    var result = kilometer * meter;
+    return result;
+}
+var convertMeter = kilometerToMeter (5);
+console.log(convertMeter);
