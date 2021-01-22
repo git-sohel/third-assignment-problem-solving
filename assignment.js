b044dc19a5e0c174d886803ce0b6713b38a951a0
git-sohel <emdsohel@gmail.com>
2021-01-22 21:42:01 +0600

    secnd is done

--- assignment.js
+++ assignment.js
@@ -2,54 +2,46 @@
 
 // kilometerToMeter function:
 
-var meter = 1000;
-function kilometerToMeter(kilometer) {
+/**function kilometerToMeter(kilometer) {    
+    var meter = 1000; // We know 1 kilometer = 1000 meter
     var result = kilometer * meter;
     return result;
 }
-var convertMeter = kilometerToMeter (5);
-console.log(convertMeter);
+var convertMeter = kilometerToMeter(5);
+console.log(convertMeter);  */
 
 
-// budgetCalcutaor function:
+// budgetCalculator function:
+
+function budgetCalculator(twoWatch, threePhone, fourLaptop) {
+    var watch = 50;
+    var phone = 100;
+    var laptop = 500;
+    var twoWatch = watch * 2;
+    var threephone = phone * 3;
+    var fourLaptop = laptop * 4;
+    var total = twoWatch + threephone + fourLaptop;
+    return total;
+}
+// var totalPrice = budgetCalculator();
+// console.log(totalPrice);
 
 
-
-//megaFriend function
 /**
- var names =["Aminul", "Sohel", "Selim", "Shohidul", "Sanowar"];
-function largestNumber(names){
-    for(var i =0; i < names.length; i++){
-        if (element.length > largest.length) {
-            largest = element;
-        }
-    }
-    
-   
-    }
-    return largest;
-    }
-
-
-var bigName = megaFriend(["Tweety", "Hedwig", ]);
-return bigName;
- */
-
- /** 
-  //megaFriend
+ //megaFriend
 
 function megaFriend(names) {
-    var maxword = names[0];
-    var largestName = "";
-    for (var i = 0; i < names.length; i++) {
-        var elements = names[i];
-        if (elements.length > names.length) {
-            maxword = elements;
-            largestName = maxword;
-        }
-        return largestName;
-    }
+   var maxword = names[0];
+   var largestName = "";
+   for (var i = 0; i < names.length; i++) {
+       var elements = names[i];
+       if (elements.length > names.length) {
+           maxword = elements;
+           largestName = maxword;
+       }
+       return largestName;
+   }
 }
 var friends = ["Aminul", "Sohel", "Selim", "Shohidul", "Sanowar"];
 console.log(friends);
-  */
+ */
